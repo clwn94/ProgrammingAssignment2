8225da6056a3a95088a220af47a73e4da80ec61b
clwn94 <clwnnnn@gmail.com>
2016-06-20 11:28:00 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## `makeCacheMatrix` creates a special "matrix", which is
-## really a list containing a function to
+## `makeCacheMatrix` creates a "matrix", which is
+##  a list containing a function to
 
 ## 1.  set the value of the matrix
 ## 2.  get the value of the matrix
